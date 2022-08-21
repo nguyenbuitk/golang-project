c514e1b5438ee5e289658d380184161b6d14ba31
nguyenbuitk <buinguyen23112k@gmail.com>
2022-08-21 15:08:47 +0700

    first commit

---
+++ cmd/todo/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"io"
+	"bufio"
+	"strings"
+	"errors"
+	// import from folder of project
+	// import ../../todo.go
+	"github.com/nguyenbuitk/todo-app"
+)
+
+//import "fmt"
+
+const (
+	todoFile = ".todos.json"
+)
+
+func main() {
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+	flag.Parse()
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(todoFile)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
+}
+
+func getInput(r io.Reader, args []string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+	return text, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nguyenbuitk/todo-app
+
+go 1.13
+
+// require github.com/alexeyco/simpletable v1.0.0
